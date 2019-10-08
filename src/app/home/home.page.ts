9a733b6c5e152bee3072c5a4bfc2aeb5ebe1a358
Alfonso Navarro <alfonso.navarro@syspotec.com>
2019-10-08 09:30:26 -0500

    Implentacion con todos los ultimos cambios y funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { ModalController } from "@ionic/angular"
+import { DetailPage } from "../detail/detail.page"
+import { OverlayEventDetail } from "@ionic/core"
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: "app-home",
+	templateUrl: "home.page.html",
+	styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+	constructor(private modalController: ModalController) {}
 
-  constructor() {}
+	async DetailPage() {
+		console.log("Detail page")
+		const modal = await this.modalController.create({
+			component: DetailPage,
+		})
 
+		modal.onDidDismiss().then((detail: OverlayEventDetail) => {
+			console.log(`The result: ${detail.data}`)
+		})
+
+		await modal.present()
+	}
 }
